ed09e54a4c0387fa356176f6a25e9c8b1f106bcf
kokou Godwin AGBEMADOKPONOU <112069533+Godark14@users.noreply.github.com>
2026-06-19 16:03:04 +0000

    exercise done

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@
 fun main() {
     // Using the provided test scores, identify the 3 students
     // with the lowest test scores
-
-    
+    val lowestScores = testScores.entries
+        .sortedBy { it.value }
+        .take(3)
+    println(lowestScores)
 }
