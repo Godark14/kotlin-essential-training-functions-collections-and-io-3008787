08b99a61c47e539650a07aec738038ef90d024bd
kokou Godwin AGBEMADOKPONOU <112069533+Godark14@users.noreply.github.com>
2026-06-22 08:56:37 +0000

    exercise done

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+import kotlin.io.path.writeLines
 
 // Prompt the user to enter the name of the test scores file
 // Read each line from the file
@@ -6,5 +8,34 @@
 // Write those 3 userId/score pairs to a new file sorted.txt
 
 fun main() {
+    print("Enter the name of the file containing scores: ")
+    val inputFileName = readLine() ?: ""
 
+    if (inputFileName.isEmpty()) {
+        println("Not a valid filename")
+        return
+    }
+
+    val inputFile = File(inputFileName)
+
+    if (!inputFile.isFile) return
+
+    val rawScores = inputFile.readLines()
+
+    val parsedScores = rawScores.map { line ->
+        val elements = line.split(":")
+        elements[0] to elements[1]
+    }
+
+    val finalScores = parsedScores
+        .sortedByDescending { it.second }
+        .take(3)
+
+    val outputPath = File("sorted.txt").toPath()
+
+    val outputScores = finalScores.map {
+        "${it.first}:${it.second}"
+    }
+
+    outputPath.writeLines(outputScores)
 }
